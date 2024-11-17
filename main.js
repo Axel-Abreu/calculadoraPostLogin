e779b51b8f33a0f05efff2f3e1609268d34d4b98
Axel-Abreu <axelsolisabreu@gmail.com>
2024-11-17 16:06:48 -0600

    se añadio el js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const $submit = document.getElementById("submit"),
+      $password = document.getElementById("password"),
+      $username = document.getElementById("username");
+
+document.addEventListener("change", (e)=>{
+    if(e.target === $submit){
+        if($password.value !== "" && $username.value !== ""){
+            e.preventDefault();
+            window.location.href = "home.html";
+            
+        }
+    }
+})
+
 
